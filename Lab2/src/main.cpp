8f6269d917e74bd8ab6583df1ba030b7b8ae8cf3
226543 <226543@student.pwr.edu.pl>
2017-03-13 22:04:13 +0100

    Update main.cpp

--- Lab2/src/main.cpp
+++ Lab2/src/main.cpp
@@ -8,9 +8,9 @@ int main() {
 
 	DArray tab;
 	struct timeval startTime, endTime;
-    double duration;
+        double duration;
     
-    gettimeofday(&startTime, NULL);
+        gettimeofday(&startTime, NULL);
 
 	for(unsigned int i = 0; i<10000; ++i) {
 		tab.addElem2(1);
@@ -19,9 +19,9 @@ int main() {
 	gettimeofday(&endTime, NULL);
 
 	duration = (endTime.tv_sec - startTime.tv_sec) * 1000.0;      // sec to ms
-    duration += (endTime.tv_usec - startTime.tv_usec) / 1000.0;   // us to ms
+        duration += (endTime.tv_usec - startTime.tv_usec) / 1000.0;   // us to ms
 
-    cout << duration << " ms \n";
+        cout << duration << " ms \n";
 	
 	return 0;
 }
