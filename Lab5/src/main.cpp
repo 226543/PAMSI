634cb1aa1ea64f1d15a4cf7a8296184ebbe4a57f
Tomasz Kaliciak <226543@student.pwr.edu.pl>
2017-04-06 17:58:37 +0200

    praca na zajeciach, nie dziala

---
+++ Lab5/src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "darray.hh"
+
+using namespace std;
+
+DArray quickSort(DArray tab,int left, int right) {
+	int temp;
+	int i = left;
+	int j = right;
+
+	int pivot = tab[size];
+	while(i <= j) {
+		while(tab[i] < pivot) {
+			++i;
+		}
+		while(tab[j] > pivot) {
+			--j;
+		}
+		if(i <= j) {
+			temp = tab[i];
+			tab[i] = tab[j];
+			tab[j] = temp;
+			++i;
+			--j;
+		}
+	}
+	if(left < j) {
+		quickSort(tab,left,j);
+	}
+	if(i < right) {
+		quickSort(tab,i,right);
+	}
+	return tab;
+}
+
+
+int main() {
+
+	DArray tab(10);
+	for(int i = 0; i <= 10; ++i) {
+		tab = i;
+	}
+	quickSort(tab,0,5);
+	
+	return 0;
+}
